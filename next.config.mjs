a35c71a3a3b85e7c487fa3cf7cdea0bfd699c65a
Karit Sittisak <karit.sittisak@gmail.com>
2024-05-03 18:17:34 +0700

    Auto Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: isProd ? '/Portfolio/' : '',
+};
 
 export default nextConfig;
