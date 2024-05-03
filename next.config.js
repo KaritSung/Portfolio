c0440909d5a2e2bc46e9c301bb2f51b8f2dbae29
Karit Sittisak <karit.sittisak@gmail.com>
2024-05-03 18:52:22 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
-/**
- * @type {import('next').NextConfig}
- */
-const isProd = process.env.NODE_ENV === 'production';
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    assetPrefix: isProd ? '/Portfolio/' : '',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+  };
+  
+  module.exports = nextConfig;
