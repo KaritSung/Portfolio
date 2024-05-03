d4fb51f8df67f7962c66a72e9fad2d6c6361f236
Karit Sittisak <karit.sittisak@gmail.com>
2024-05-03 19:58:29 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-/**
-* @type {import('next').NextConfig}
-*/
 const nextConfig = {
-  output: "export",
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "./",
-};
-
-export default nextConfig;
+    output: 'export',
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
+  
